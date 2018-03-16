4ecf5d9e095ab4a15d65f1a2983c91106a3ade6b
Stephen Adams <stephenpauladams@gmail.com>
2018-03-15 23:36:32 -0400

    Added more properties from their API documentation to the webhook and error response model
    Still need to figure out what kind of witchcraft they're doing with the
    error response and whether it should be added to every response model
    (or to the base response?)

--- source/SixFourThree.SezzleSharp/SixFourThree.SezzleSharp/Models/OrderWebhook.cs
+++ source/SixFourThree.SezzleSharp/SixFourThree.SezzleSharp/Models/OrderWebhook.cs
@@ -4,7 +4,53 @@ using System.Text;
 
 namespace SixFourThree.SezzleSharp.Models
 {
-    class OrderWebhook
+    /// <summary>
+    /// Because the majority of a consumer’s checkout process happens on Sezzle’s pages, our API uses webhooks to communicate information about checkout updates, completions, or refunds to your system.
+    /// We expect any response in the 200 range on submitting webhooks.
+    /// </summary>
+    public class OrderWebhook
     {
+        /// <summary>
+        /// The time (UTC) at which the Webhook was generated.
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// A unique identifier for the webhook.
+        /// </summary>
+        public string Uuid { get; set; }
+
+        /// <summary>
+        /// The high-level category. For example, order_update
+        /// </summary>
+        /// <remarks>
+        /// Might want to cast this to an enumeration using a JSON serializer. Currently only order_update is a possibility, but it's better than using magic strings.
+        /// </remarks>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// The specific action. For example, order_complete or order_refund.
+        /// </summary>
+        /// <remarks>
+        /// Might want to cast this to an enumeration using a JSON serializer. Possible values are:
+        /// order_complete - The checkout was completed successfully
+        /// order_refund - The order was refunded from the Sezzle Merchant Dashboard
+        /// </remarks>
+        public string Event { get; set; }
+
+        /// <summary>
+        /// The ID for the Checkout/Order.
+        /// </summary>
+        public string ObjectUuid { get; set; }
+
+        /// <summary>
+        /// Unique ID for a refund. Included if the webhook event is order_refund.
+        /// </summary>
+        public string RefundId { get; set; }
+
+        /// <summary>
+        /// Price object. Included if the webhook event is order_refund.
+        /// </summary>
+        public Price RefundAmount { get; set; }
     }
 }
--- source/SixFourThree.SezzleSharp/SixFourThree.SezzleSharp/Models/Responses/ErrorResponse.cs
+++ source/SixFourThree.SezzleSharp/SixFourThree.SezzleSharp/Models/Responses/ErrorResponse.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace SixFourThree.SezzleSharp.Models.Responses
 {
-    class ErrorResponse
+    /// <summary>
+    /// Unless otherwise specified in our documentation, Sezzle returns a standard API error object.
+    /// We attempt to keep these errors as consistent as possible, and will announce any changes in advance if they are required.
+    /// </summary>
+    public class ErrorResponse
     {
+        /// <summary>
+        /// Matches the HTTP Status code of the response
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// A programmatic identifier for the error. These rarely (if at all) change
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        //  A human-friendly string. These may change, and are intended to assist in debugging rather than program logic.
+        /// </summary>
+        public string Message { get; set; }
     }
 }
